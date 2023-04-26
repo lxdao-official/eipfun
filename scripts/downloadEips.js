d185639fded67d03a21cccbaed67509e87a60e49
Neal <1107554021@qq.com>
2023-04-26 14:40:27 +0800

    feta: clone the latest data before building

---
+++ scripts/downloadEips.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const download = require('download-git-repo');
+
+function deleteFolder(filePath) {
+  if (fs.existsSync(filePath)) {
+    const files = fs.readdirSync(filePath);
+    files.forEach((file) => {
+      const nextFilePath = `${filePath}/${file}`;
+      const states = fs.statSync(nextFilePath);
+      if (states.isDirectory()) {
+        //recurse
+        deleteFolder(nextFilePath);
+      } else {
+        //delete file
+        fs.unlinkSync(nextFilePath);
+      }
+    });
+    fs.rmdirSync(filePath);
+  }
+}
+
+const paths = './ETH-EIPs/';
+
+~(async function () {
+  deleteFolder(paths);
+  download(
+    'direct:https://github.com/ethereum/EIPs.git',
+    'ETH-EIPs',
+    { clone: true },
+    (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+      console.log('clone success');
+    }
+  );
+})();
