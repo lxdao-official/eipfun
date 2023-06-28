5eb84a222f7e35886f24c84335916900a179f368
Neal <1107554021@qq.com>
2023-06-28 09:32:40 +0800

    feat: update text

--- components/details/Video.tsx
+++ components/details/Video.tsx
@@ -38,7 +38,7 @@ export default function Video({ list }: { list: string[] }) {
           lineHeight={'20px'}
           mb={1}
         >
-          No content, go to update the document
+          Any one may contribute to propose adopted projects.
         </Typography>
         <Link
           display={'block'}
@@ -48,7 +48,7 @@ export default function Video({ list }: { list: string[] }) {
           fontSize="15px"
           underline="none"
         >
-          Get start
+          Go propose
           <EastIcon
             sx={{
               width: 14,
