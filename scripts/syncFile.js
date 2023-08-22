fdc2030da4b231459d62d2fd20e30d16fafd888b
Neal <1107554021@qq.com>
2023-08-22 14:44:23 +0800

    docs: docs update 2023-08-22

--- scripts/syncFile.js
+++ scripts/syncFile.js
@@ -16,7 +16,7 @@ files.forEach((file) => {
   } else {
     fs.writeFile(
       `./content/en/${file}`,
-      `## Videos\n\n- [Example Video Title](https://www.youtube.com/watch?v=TDGq4aeevgY)\n\n## Projects\n\n- [Example Project Name](https://xxxx.xxx/xxxxx) Project Description (one sentence) ![this is an img's alt for the project logo](https://xxxx.xxx/project-logo.xxx)\n\n## Further Reading\n\n- [Example Article Title](https://xxxx.xxx/xxxxx) ![this is an img's alt for the article cover](https://xxxx.xxx/article-cover.xxx)\n`,
+      `## Videos\n\n- [Example Video Title](https://www.youtube.com/watch?v=TDGq4aeevgY)\n\n## Projects\n\n- [Example Project Name](https://xxxx.xxx/xxxxx) Project Description (one sentence) ![this is an img's alt for the project logo](https://xxxx.xxx/project-logo.xxx)\n\n## Further Reading\n\n- [Example Article Title](https://xxxx.xxx/xxxxx)\n`,
       function (err) {
         if (err) console.log(err);
       }
@@ -28,7 +28,7 @@ files.forEach((file) => {
   } else {
     fs.writeFile(
       `./content/zh/${file}`,
-      `## Videos\n\n- [Example Video Title](https://www.youtube.com/watch?v=TDGq4aeevgY)\n\n## Projects\n\n- [Example Project Name](https://xxxx.xxx/xxxxx) Project Description (one sentence) ![this is an img's alt for the project logo](https://xxxx.xxx/project-logo.xxx)\n\n## Further Reading\n\n- [Example Article Title](https://xxxx.xxx/xxxxx) ![this is an img's alt for the article cover](https://xxxx.xxx/article-cover.xxx)\n`,
+      `## Videos\n\n- [Example Video Title](https://www.youtube.com/watch?v=TDGq4aeevgY)\n\n## Projects\n\n- [Example Project Name](https://xxxx.xxx/xxxxx) Project Description (one sentence) ![this is an img's alt for the project logo](https://xxxx.xxx/project-logo.xxx)\n\n## Further Reading\n\n- [Example Article Title](https://xxxx.xxx/xxxxx)\n`,
       function (err) {
         if (err) console.log(err);
       }
